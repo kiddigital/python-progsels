1b208f2a62a7affb629073d84225cffab15a7105
kiddigital <kiddigital@gmail.com>
2017-11-29 15:53:18 +0100

    [WIP]Atlassion activity script

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # python-progsels
 
-to-do
+## Atlassian Activity Stream reader
+SImple script that reads the Activity stream of a JIRA and/or Confluence environment
+for a give user and creates a simple (per date, deduplicated) overview of registered 
+activity.
+
---
+++ atlassian-activity-streamreader/activity.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python3
+
+import requests
+import feedparser
+import lxml.html
+
+URL = 'https://jira.vancis.nl/activity'
+USER = 'patrickb'
+PASS = '<mysecret>'
+
+params = {'os_authType': 'basic', 'streams': 'user IS '+USER, 'maxResults': 200}
+mylist = []
+
+response = requests.get(URL, auth=(USER, PASS), params=params)
+
+feed = feedparser.parse(response.content)
+
+# print(feed['feed']['title'])
+
+for entry in feed['entries']:
+    app = entry['atlassian_application']
+    try:
+        link = lxml.html.fromstring(entry['link']).text_content().replace('\n', '#')
+    except Exception:
+        link = '!!!'
+    try:
+        summary = lxml.html.fromstring(entry['summary']).text_content().replace('\n', '#')
+    except Exception:
+        summary = '!!!'
+    mylist.append(entry['updated'][1:10] + ' ' + link + ' ' + summary)
+    # print(entry['activity_verb'])
+
+mylist = reversed(sorted(set(mylist)))
+
+for myline in mylist:
+    print(myline)
